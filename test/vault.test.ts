4b2e464cce60aba6de8e39d21b79aa3c060823b2
SalongDb <143243056+SalongDb@users.noreply.github.com>
2026-02-22 22:40:01 +0530

    feat: add test for reject zero deposit

--- test/vault.test.ts
+++ test/vault.test.ts
@@ -2,11 +2,11 @@ import { describe, it } from "node:test";
 import hre from "hardhat";
 import assert from "node:assert";
 
-const { viem} = await hre.network.connect();
+const { viem } = await hre.network.connect();
 
 describe("Vault", function () {
 
-    it("Should accept deposits", async function() {
+    it("Should accept deposits", async function () {
 
         const [deployer, user] = await viem.getWalletClients();
 
@@ -21,5 +21,21 @@ describe("Vault", function () {
 
         assert.equal(depoInfo[0], 10n ** 18n);
     });
-    
+
+    it("Should reject zero deposit", async function () {
+
+        const [deployer, user] = await viem.getWalletClients();
+
+        const vault = await viem.deployContract("Vault", []);
+
+        await assert.rejects(
+            vault.write.deposit({
+                account: user.account,
+                value: 0n
+            }),
+            {
+                message: /Send some ETH/
+            }
+        );
+    });
 });
