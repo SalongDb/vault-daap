71ab61fc73f0bc3c7fe5c9bc77a18e543a0240d7
SalongDb <143243056+SalongDb@users.noreply.github.com>
2026-02-23 18:22:58 +0530

    feat: add test for deposits and withdrawls

--- test/vault.test.ts
+++ test/vault.test.ts
@@ -1,8 +1,10 @@
 import { describe, it } from "node:test";
 import hre from "hardhat";
 import assert from "node:assert";
+import { deployContract } from "viem/zksync";
+import strict from "node:assert/strict";
 
-const { viem } = await hre.network.connect();
+const { viem, networkHelpers } = await hre.network.connect();
 
 describe("Vault", function () {
 
@@ -36,6 +38,114 @@ describe("Vault", function () {
             {
                 message: /Send some ETH/
             }
+        );
+    });
+
+    it("Should revert withdraw before 7 days", async function () {
+
+        const [owner, user] = await viem.getWalletClients();
+
+        const vault = await viem.deployContract("Vault");
+
+        await vault.write.deposit({
+            account: user.account,
+            value: 10n ** 18n
+        });
+
+        await assert.rejects(
+            vault.write.withdraw(
+                [10n ** 18n],
+                { account: user.account }),
+            {
+                message: /7 days not completed/
+            }
+        );
+    })
+
+    it("Should deduct 2% when withdrawn", async function () {
+        const [owner, user] = await viem.getWalletClients();
+        const vault = await viem.deployContract("Vault");
+
+        await vault.write.deposit({
+            account: user.account,
+            value: 10n ** 18n
+        });
+
+        await networkHelpers.time.increase(7 * 24 * 60 * 60);
+        await networkHelpers.mine();
+
+        const feeBalanceBefore = await vault.read.feeBalance();
+
+        await vault.write.withdraw(
+            [10n ** 18n],
+            { account: user.account }
+        );
+
+        const feeBalanceAfter = await vault.read.feeBalance();
+
+        const expectedFee = 2n * 10n ** 16n;
+
+        assert.strictEqual(
+            feeBalanceAfter,
+            feeBalanceBefore + expectedFee
+        );
+    })
+
+    it("should allow only owner to withdraw fees", async function () {
+        const [owner, user] = await viem.getWalletClients();
+        const vault = await viem.deployContract("Vault");
+
+        await vault.write.deposit({
+            account: user.account,
+            value: 10n ** 18n
+        });
+
+        await networkHelpers.time.increase(7 * 24 * 60 * 60);
+        await networkHelpers.mine();
+
+        await vault.write.withdraw(
+            [10n ** 18n],
+            { account: user.account }
+        );
+
+        const feeBalance = await vault.read.feeBalance();
+
+        await vault.write.feeWithdraw(
+            [feeBalance],
+            { account: owner.account }
+        );
+
+        const newFeeBalance = await vault.read.feeBalance();
+
+        assert.strictEqual(newFeeBalance, 0n);
+    })
+
+    it("Should revert if non-owner tries to withdraw fees", async function () {
+        const [owner, user] = await viem.getWalletClients();
+        const vault = await viem.deployContract("Vault", []);
+
+        // Generate fee first
+        await vault.write.deposit({
+            account: user.account,
+            value: 10n ** 18n
+        });
+
+        await networkHelpers.time.increase(7 * 24 * 60 * 60 + 1);
+
+        await vault.write.withdraw(
+            [10n ** 18n],
+            { account: user.account }
+        );
+
+        const feeBalance = await vault.read.feeBalance();
+
+        // Non-owner attempt
+        await assert.rejects(
+            vault.write.feeWithdraw(
+                [feeBalance],
+                { account: user.account }
+            ),
+            /Ownable/
         );
     });
 });
